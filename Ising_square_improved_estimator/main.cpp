d11d6cf3d0d922e75285a6c1148020dc06682bec
ADE <ackielilac.g1@gmail.com>
2026-01-15 14:26:56 +0900

    Initial Commit

---
+++ Ising_square_improved_estimator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ising_square_improved_estimator
+//
+//  Created by Akihisa Tateishi on 2026/01/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
